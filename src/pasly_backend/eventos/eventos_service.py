7d0d7f33a0090eb10738c4e385da58363577cb3b
Teo <Teobenjaminmoravicki@gmail.com>
2026-09-03 11:42:38 -0300

    Arreglar problema de __pycache__

--- src/pasly_backend/eventos/eventos_service.py
+++
@@ -1,7 +0,0 @@
-class EventosService:
-
-    def get_events(self):
-        return {"message": "Lista de eventos"}
-
-    def get_event(self, event_id: int):
-        return {"message": f"Evento {event_id}"}
